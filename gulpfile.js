1abe68c281188ea24394126268cf601e6ef071f0
Jordan Gensler <jordan.gensler@nike.com>
2015-08-19 21:40:41 -0700

    Reverting back to not reading files in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('lint', function() {
 });
 
 gulp.task('test', function () {
-  return gulp.src('test/**/*.test.js')
+  return gulp.src('test/**/*.test.js', {read: false})
     .pipe(mocha({
       reporter: 'spec',
       compilers: {
