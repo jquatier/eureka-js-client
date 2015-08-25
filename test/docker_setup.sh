903c0cfea8ef14c8d4785cf501ce3bcfecb93cd3
Jordan Gensler <jordan.gensler@nike.com>
2015-08-25 13:49:03 -0700

    Missing http?

--- test/docker_setup.sh
+++ test/docker_setup.sh
@@ -1,4 +1,4 @@
 docker pull netflixoss/eureka:1.1.147
 docker run -d --name eureka -p 127.0.0.1:8080:39657 netflixoss/eureka:1.1.147
 docker ps -a
-curl -v 127.0.0.1:39657/eureka/
+curl -v http://127.0.0.1:39657/eureka/
