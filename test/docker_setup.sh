798358c6071fdb722dc9be60d31ac4c0d8819574
Jordan Gensler <jordan.gensler@nike.com>
2015-08-25 20:37:56 -0700

    trying port

--- test/docker_setup.sh
+++ test/docker_setup.sh
@@ -1,5 +1,6 @@
 docker pull netflixoss/eureka:1.1.147
 docker run -d --name eureka -P netflixoss/eureka:1.1.147
 docker ps -a
+docker port eureka
 curl -v http://localhost:39657/eureka/
 curl -v http://localhost:8080/eureka/
