daf88761ee9ca73c6df5d36761d51010af3e27f6
Andrei Kazakou <akazakou@exadel.com>
2018-07-11 13:52:58 +0300

    Added comments and additional exports with type hints
    Signed-off-by: Andrei Kazakou <akazakou@exadel.com>

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,44 +1,122 @@
-import { Options as RequestOptions } from 'request'
+import { Options as RequestOptions, Response } from 'request'
 import { EventEmitter } from 'events'
 
 class EurekaClient extends EventEmitter {
+  /**
+   * Helper method to get the instance ID. If the datacenter is AWS, this will be the instance-id in the metadata. Else, it's the hostName.
+   */
   readonly instanceId: string
+  /**
+   * Helper method to determine if this is an AWS datacenter.
+   */
   readonly amazonDataCenter: boolean
 
   constructor(config: Config)
 
+  /**
+   * Registers instance with Eureka, begins heartbeats, and fetches registry.
+   * @param {(err: Error, ...rest: any[]) => void} callback
+   */
   start(callback: (err: Error, ...rest: any[]) => void)
 
+  /**
+   * De-registers instance with Eureka, stops heartbeats / registry fetches.
+   * @param {(err: Error, ...rest: any[]) => void} callback
+   */
   stop(callback: (err: Error, ...rest: any[]) => void)
 
+  /**
+   * Validates client configuration.
+   * @param {Config} config
+   */
   validateConfig(config: Config): void
 
+  /**
+   * Registers with the Eureka server and initializes heartbeats on registration success.
+   * @param {(err: Error, ...rest: any[]) => any} callback
+   * @returns {any}
+   */
   register(callback: (err: Error, ...rest: any[]) => any): any
 
+  /**
+   * De-registers with the Eureka server and stops heartbeats.
+   * @param {(err: Error, ...rest: any[]) => any} callback
+   * @returns {any}
+   */
   deregister(callback: (err: Error, ...rest: any[]) => any): any
 
+  /**
+   * Sets up heartbeats on interval for the life of the application.
+   * Heartbeat interval by setting configuration property: eureka.heartbeatInterval
+   */
   startHeartbeats(): void
 
   renew(): void
 
+  /**
+   * Sets up registry fetches on interval for the life of the application.
+   * Registry fetch interval setting configuration property: eureka.registryFetchInterval
+   */
   startRegistryFetches(): void
 
+  /**
+   * Retrieves a list of instances from Eureka server given an appId
+   * @param {string} appId
+   * @returns {InstanceConfig[]}
+   */
   getInstancesByAppId(appId: string): InstanceConfig[]
 
+  /**
+   * Retrieves a list of instances from Eureka server given a vipAddress
+   * @param vipAddress
+   * @returns {InstanceConfig[]}
+   */
   getInstancesByVipAddress(vipAddress: any): InstanceConfig[]
 
+  /**
+   * Orchestrates fetching registry
+   * @param {(err: Error, ...rest: any[]) => void} callback
+   */
   fetchRegistry(callback: (err: Error, ...rest: any[]) => void): void
 
+  /**
+   * Retrieves all applications registered with the Eureka server
+   * @param {(err: Error, ...rest: any[]) => void} callback
+   */
   fetchFullRegistry(callback: (err: Error, ...rest: any[]) => void): void
 
+  /**
+   * Retrieves all applications registered with the Eureka server
+   * @param {(err: Error, ...rest: any[]) => void} callback
+   */
   fetchDelta(callback: (err: Error, ...rest: any[]) => void): void
 
+  /**
+   * Transforms the given registry and caches the registry locally
+   * @param {EurekaAppsResponse} registry
+   */
   transformRegistry(registry: EurekaAppsResponse): void
 
+  /**
+   * Transforms the given application and places in client cache. If an application
+   * has a single instance, the instance is placed into the cache as an array of one
+   * @param {App} app
+   * @param {Cache} cache
+   */
   transformApp(app: App, cache: Cache): void
 
+  /**
+   * Returns true if instance filtering is disabled, or if the instance is UP
+   * @param {InstanceConfig} instance
+   * @returns {boolean}
+   */
   validateInstance(instance: InstanceConfig): boolean
 
+  /**
+   * Returns an array of vipAddresses from string vipAddress given by eureka
+   * @param {string} vipAddress
+   * @returns {string[]}
+   */
   splitVipAddress(vipAddress: string): string[]
 
   handleDelta(cache: Cache, appDelta: EurekaDeltaResponse): void
@@ -49,12 +127,42 @@ class EurekaClient extends EventEmitter {
 
   deleteInstance(cache: Cache, instance: InstanceConfig): void
 
+  /**
+   * Fetches the metadata using the built-in client and updates the instance
+   * configuration with the hostname and IP address. If the value of the config
+   * option 'eureka.useLocalMetadata' is true, then the local IP address and
+   * hostname is used. Otherwise, the public IP address and hostname is used. If
+   * 'eureka.preferIpAddress' is true, the IP address will be used as the hostname.
+   *
+   * A string replacement is done on the healthCheckUrl, statusPageUrl and
+   * homePageUrl so that users can define the URLs with a placeholder for the
+   * host ('__HOST__'). This allows flexibility since the host isn't known until
+   * the metadata is fetched. The replaced value respects the config option
+   * 'eureka.useLocalMetadata' as described above.
+   *
+   * This will only get called when dataCenterInfo.name is Amazon, but you can
+   * set config.eureka.fetchMetadata to false if you want to provide your own
+   * metadata in AWS environments.
+   * @param {() => void} callback
+   */
   addInstanceMetadata(callback: () => void): void
 
-  eurekaRequest(opts: RequestOptions, callback: (error: Error, response, body) => void, retryAttempt?: any): void
+  /**
+   * Helper method for making a request to the Eureka server. Handles resolving
+   * the current cluster as well as some default options.
+   * @param {request.Options} opts
+   * @param {(error: Error, response: request.Response, body: any) => void} callback
+   * @param {number} retryAttempt
+   */
+  eurekaRequest(
+    opts: RequestOptions, callback: (error: Error, response: Response, body: any) => void, retryAttempt?: number): void
 }
 
 interface Logger {
+  /**
+   * Set default error logger level
+   * @param {number | "error" | "warn" | "info" | "debug"} inVal
+   */
   level(inVal: number | 'error' | 'warn' | 'info' | 'debug'): void
 
   error(...args: any[]): void
@@ -165,12 +273,16 @@ type LeaseInfo = {
   evictionTimestamp?: number,
   serviceUpTimestamp?: number,
 }
-
 type PortWrapper = number | {
+  /**
+   * Checks whether a port is enabled for traffic or not.
+   *
+   * @param type indicates whether it is secure or non-secure port.
+   * @return true if the port is enabled, false otherwise.
+   */
   '@enabled': boolean,
   $: number,
 }
-
 type InstanceStatus = 'UP' | 'DOWN' | 'STARTING' | 'OUT_OF_SERVICE' | 'UNKNOWN';
 type DataCenterInfo = {
   '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
@@ -184,30 +296,134 @@ type DataCenterInfo = {
 }
 
 interface InstanceConfig {
+  /**
+   * Sets the unique id of the instance.
+   * (Note) now that id is set at creation time within the instanceProvider, why do the other checks?
+   * This is still necessary for backwards compatibility when upgrading in a deployment with multiple
+   * client versions (some with the change, some without).
+   */
   instanceId?: string,
+  /**
+   * Sets the name of the application registering with discovery
+   */
   app: string,
+  /**
+   * Sets the group name of the application registering with discovery
+   */
   appGroupName?: string,
+  /**
+   * Returns the ip address of the instance.
+   */
   ipAddr?: string,
+  /**
+   * Sets the identity of this application instance
+   *
+   * @deprecated
+   */
   sid?: string,
+  /**
+   * Sets the port number that is used for servicing requests.
+   */
   port: PortWrapper,
+  /**
+   * Sets the secure port that is used for servicing requests.
+   */
   securePort?: PortWrapper,
+  /**
+   * Sets the home page set for this instance.
+   */
   homePageUrl?: string,
+  /**
+   * Sets the status page set for this instance.
+   */
   statusPageUrl?: string,
+  /**
+   * Sets the absolute URLs for the health check page for both secure and
+   * non-secure protocols. If the port is not enabled then the URL is
+   * excluded.
+   */
   healthCheckUrl?: string,
+  /**
+   * Sets the absolute URL for the health check page for both secure and
+   * non-secure protocols. If the port is not enabled then the URL is
+   * excluded.
+   */
   secureHealthCheckUrl?: string,
+  /**
+   * Sets the Virtual Internet Protocol address for this instance. Defaults to
+   * hostname if not specified.
+   */
   vipAddress: string,
+  /**
+   * Sets the Secure Virtual Internet Protocol address for this instance.
+   * Defaults to hostname if not specified.
+   */
   secureVipAddress?: string,
+  /**
+   * @Deprecated
+   */
   countryId?: number,
+  /**
+   * Sets data center information identifying if it is AWS or not.
+   *
+   * @return the data center information.
+   */
   dataCenterInfo: DataCenterInfo,
+  /**
+   * Sets the default network address to connect to this instance. Typically this would be the fully
+   * qualified public hostname.
+   *
+   * However the user can configure the {@link EurekaInstanceConfig} to change the default value used
+   * to populate this field using the {@link EurekaInstanceConfig#getDefaultAddressResolutionOrder()} property.
+   *
+   * If a use case need more specific hostnames or ips, please use data from {@link #getDataCenterInfo()}.
+   *
+   * For legacy reasons, it is difficult to introduce a new address-type field that is agnostic to hostname/ip.
+   */
   hostName?: string,
+  /**
+   * Sets the status of the instance.
+   */
   status?: InstanceStatus,
+  /**
+   * Sets the overridden status if any of the instance.
+   *
+   * @deprecated
+   */
   overriddenstatus?: InstanceStatus,
+  /**
+   * Sets the overridden status if any of the instance.
+   */
   overriddenStatus?: InstanceStatus,
+  /**
+   * Sets the lease information regarding when it expires.
+   */
   leaseInfo?: LeaseInfo,
+  /**
+   * Sets a flag if this instance is the same as the discovery server that is
+   * return the instances. This flag is used by the discovery clients to
+   * identity the discovery server which is coordinating/returning the
+   * information.
+   */
   isCoordinatingDiscoveryServer?: boolean,
+  /**
+   * Sets all application specific metadata set on the instance
+   */
   metadata?: object,
+  /**
+   * Sets the time elapsed since epoch since the instance status has been
+   * last updated.
+   */
   lastUpdatedTimestamp?: number,
+  /**
+   * Sets the last time stamp when this instance was touched.
+   */
   lastDirtyTimestamp?: number,
+  /**
+   * Sets the type of action done on the instance in the server.Primarily
+   * used for updating deltas in the {@link com.netflix.discovery.EurekaClient}
+   * instance.
+   */
   actionType?: 'ADDED' | 'MODIFIED' | 'DELETED',
 }
 
@@ -297,7 +513,11 @@ export {
   EurekaAppsResponse,
   App,
   EurekaDeltaResponse,
-  Cache
+  Cache,
+
+  RequestOptions,
+  Response,
+  EventEmitter
 }
 
 export default EurekaClient
